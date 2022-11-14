86e7a9e4683b66e5b8f1a2116fb8e9c88e241ae9
Данил <d4nssd@yandex.com>
2022-11-14 15:08:20 +0300

    task1 commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,29 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        binding.btnSearch.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(p0: View?) {
+//                Toast.makeText(this@MainActivity, "Нажали на кнопку поиск", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
+        binding.btnSearch.setOnClickListener {
+            Intent(this@MainActivity, SearchActivity::class.java).also { startActivity(it) }
+        }
+        
+//        binding.btnLibrary.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(p0: View?) {
+//                Toast.makeText(this@MainActivity, "Нажали на кнопку медиатека", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
+        binding.btnLibrary.setOnClickListener {
+            Intent(this@MainActivity, LibraryActivity::class.java).also { startActivity(it) }
+        }
+
+        binding.btnSettings.setOnClickListener {
+            Intent(this@MainActivity, SettingsActivity::class.java).also { startActivity(it) }
+        }
     }
 
     override fun onDestroy() {
