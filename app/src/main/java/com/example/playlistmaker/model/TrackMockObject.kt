0a93d5874a9a252032354860dbc26ef38d6242c7
Данил <d4nssd@yandex.com>
2023-01-28 19:59:28 +0300

    implement TrackListAdapter and connect to recyclerView | using library Glide to load images from the internet | some layout and toolbar corrections

--- app/src/main/java/com/example/playlistmaker/model/TrackMockObject.kt
+++ app/src/main/java/com/example/playlistmaker/model/TrackMockObject.kt
@@ -41,6 +41,12 @@ object TrackMockObject {
             artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
         ))
 
-        return list
+        val tracks = mutableListOf<Track>()
+
+        for(i in 0..25) {
+            tracks.add(list.random())
+        }
+
+        return tracks
     }
 }
