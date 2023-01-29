64e02ed1071f629c4a3f202ae68666cb80be33c4
Данил <d4nssd@yandex.com>
2023-01-29 18:30:24 +0300

    corrections after review

--- app/src/main/java/com/example/playlistmaker/adapter/TrackListAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/adapter/TrackListAdapter.kt
@@ -9,29 +9,7 @@ import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.TrackItemBinding
 import com.example.playlistmaker.model.Track
 
-class TrackViewHolder(private val binding: TrackItemBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(track: Track) {
-        binding.apply {
-            tvTrackName.text = track.trackName
-            tvArtistNameAndTrackTime.text = itemView.context.getString(
-                R.string.artist_name_and_track_time,
-                track.artistName,
-                track.trackTime
-            )
-            Glide.with(itemView)
-                .load(track.artworkUrl100)
-                .transform(RoundedCorners(ARTWORK_CORNER_RADIUS))
-                .placeholder(R.drawable.ic_track_artwork_placeholder)
-                .into(ivArtwork)
-        }
-    }
-
-    private companion object {
-        const val ARTWORK_CORNER_RADIUS = 10
-    }
-}
-
-class TrackListAdapter : RecyclerView.Adapter<TrackViewHolder>() {
+class TrackListAdapter : RecyclerView.Adapter<TrackListAdapter.TrackViewHolder>() {
     var tracks: List<Track> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
@@ -44,4 +22,26 @@ class TrackListAdapter : RecyclerView.Adapter<TrackViewHolder>() {
     }
 
     override fun getItemCount(): Int = tracks.size
+
+    class TrackViewHolder(private val binding: TrackItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(track: Track) {
+            binding.apply {
+                tvTrackName.text = track.trackName
+                tvArtistNameAndTrackTime.text = itemView.context.getString(
+                    R.string.artist_name_and_track_time,
+                    track.artistName,
+                    track.trackTime
+                )
+                Glide.with(itemView)
+                    .load(track.artworkUrl100)
+                    .transform(RoundedCorners(ARTWORK_CORNER_RADIUS))
+                    .placeholder(R.drawable.ic_track_artwork_placeholder)
+                    .into(ivArtwork)
+            }
+        }
+
+        private companion object {
+            const val ARTWORK_CORNER_RADIUS = 10
+        }
+    }
 }
